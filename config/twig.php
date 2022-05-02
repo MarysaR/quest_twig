b966d4db08f82398e2c38d167c3efccdcb3ac150
Mary <regentmarysapro@gmail>
2022-05-02 11:03:26 +0200

    quete twig 03 - Globale

--- config/twig.php
+++ config/twig.php
@@ -5,3 +5,8 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $loader = new Twig\Loader\FilesystemLoader(__DIR__ . '/../src/View');
 $twig = new Twig\Environment($loader, ['debug' => true]);
 $twig->addExtension(new Twig\Extension\DebugExtension());
+
+//On ajoute la globale adresse mail de contact à l'initialisation de l'objet Twig
+
+$contactEmail = 'support@wildcodeschool.com';
+$twig->addGlobal('contactEmail', $contactEmail);
